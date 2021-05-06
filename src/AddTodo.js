c016882a65a2ae9c88e929726bc41881d1f30092
Dmitry Vasilevsky <divasilevski@gmail.com>
2021-05-06 16:05:42 +0300

    Form validation

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import { StyleSheet, TextInput, View, Button } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, TextInput, View, Button, Alert } from 'react-native'
 
 export const AddTodo = ({ onSubmit }) => {
+  const [value, setValue] = useState('')
+
   const pressHandler = () => {
-    onSubmit('Test todo')
+    if (value.trim()) {
+      onSubmit(value)
+      setValue('')
+    } else {
+      Alert.alert('Название дела не может быть пустым!')
+    }
   }
 
   return (
     <View style={styles.block}>
-      <TextInput style={styles.input} />
+      <TextInput
+        style={styles.input}
+        onChangeText={setValue}
+        value={value}
+        placeholder="Введите название дела..."
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
       <Button title="Добавить" onPress={pressHandler} />
     </View>
   )
